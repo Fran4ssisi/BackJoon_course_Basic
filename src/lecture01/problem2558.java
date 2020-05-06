fc6d956b9169e081e6f828811f8739923dcdf0d4
Fran4ssisi <carloes87@gmail.com>
2020-05-06 21:16:15 +0900

    기초 알고리즘 훈련 1일차

---
+++ src/lecture01/problem2558.java
@@ -0,0 +1,17 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class problem2558 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
---
+++ src/lecture01/problen10950.java
@@ -0,0 +1,18 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class problen10950 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while(tc-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
